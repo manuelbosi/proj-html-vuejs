102fd95c08943d4b0c970c58d07cfc3721bb0202
Manuel Bosi <manuelbosi57@gmail.com>
2020-12-09 13:04:46 +0100

    inserimento commenti html e vue js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Istanza vue js
+// Creare struttura dati: link header, link footer
+// Un array di stringhe headerLink : ["link1", "link2", "link3", "link4", "link5", "link6", "link7"];
+// Un array di oggetti footerLink
+/*
+footerLink : [
+  {title: "Titolo1", text: ["paragrafo"]},
+  {title: "Titolo1", links: ["link1", "link2", "link3", "link4"]},
+  {title: "Titolo1", links: ["link1", "link2", "link3", "link4"]},
+  {title: "Titolo1", links: ["link1", "link2", "link3", "link4"]}
+];
+*/
