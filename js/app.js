e7853adbc99ee126f253e8a44f6bf711d8e90232
Manuel Bosi <manuelbosi57@gmail.com>
2020-12-09 16:09:48 +0100

    separate istanze vue stampa link footer

--- js/app.js
+++ js/app.js
@@ -10,11 +10,27 @@ footerLink : [
   {title: "Titolo1", links: ["link1", "link2", "link3", "link4"]}
 ];
 */
-const app = new Vue({
-  el : "#app",
+
+// ISTANZA VUE PER HEADER
+const header = new Vue({
+  el : "#headerApp",
   data : {
-    logoPath: "img/logo.png",
-    headerLink: ["Home", "Apple", "Microsoft", "Android", "Forums", "Contact us", "JOIN US"],
-    searchIcon: "fas fa-search"
+    logoPath : "img/logo.png",
+    headerLinks : ["Home", "Apple", "Microsoft", "Android", "Forums", "Contact us", "JOIN US"],
+    searchIcon : "fas fa-search"
+  }
+});
+
+// ISTANZA VUE PER FOOTER
+const footer = new Vue({
+  el : "#footerApp",
+  data : {
+    firstColumn: { title: "Titolo1", text: "paragrafo" },
+    footerLinks :
+      [
+        { title: "Titolo2", links: ["link2.1", "link2.2", "link2.3", "link2.4"] },
+        { title: "Titolo3", links: ["link3.1", "link3.2", "link3.3", "link3.4"] },
+        { title: "Titolo4", links: ["link4.1", "link4.2", "link4.3", "link4.4"] }
+      ]
   }
 });
